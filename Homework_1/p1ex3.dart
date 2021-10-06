c57cddba276314a5eb0e1173a560c4ddea9a6749
victoriaraznodumova <90800890+victoriaraznodumova@users.noreply.github.com>
2021-10-06 03:54:12 +0300

    1  3

--- Homework_1/p1ex3.dart
+++ Homework_1/p1ex3.dart
@@ -1,6 +1,29 @@
 import 'dart:io';
 
 void main() {
+  List<int> list = [];
+  stdin.readLineSync()!.split(' ').forEach((element) {
+    list.add(int.parse(element));
+  });
+  if (list[0] >= 0) {
+    if (list[0] < 10) {
+      print('DIGIT');
+    }
+  }
+
+  if (list[0] > 9) {
+    if (list[0] < 100){
+      print('NUM');
+    }
+  }
+  else {
+    print('OTHER');
+  }
+  }
 
 
-}
+
+
+
+
+
---
+++ Homework_1/p1ex4.dart
@@ -0,0 +1,15 @@
+import 'dart:io';
+
+void main() {
+  List<int> list = [];
+  stdin.readLineSync()!.split(' ').forEach((element) {
+    list.add(int.parse(element));
+  });
+
+  if(list[0] > list[1]){
+    int d = list[1];
+    int o;
+    while d>0 {
+
+    }
+  }
