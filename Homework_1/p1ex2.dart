70df4cebf1a37c628475b58b76c703263eea9ce2
victoriaraznodumova <90800890+victoriaraznodumova@users.noreply.github.com>
2021-10-06 03:26:25 +0300

    1  2

---
+++ Homework_1/p1ex2.dart
@@ -0,0 +1,9 @@
+import 'dart:io';
+
+void main() {
+  List<int> list = [];
+  stdin.readLineSync()!.split(' ').forEach((element) {
+    list.add(int.parse(element));
+  });
+  print(list[0]*list[0] + list[1]*list[1]);
+}
